cfe25ac417cb723cc72d032d456a9e4cfd2f3a56
steven <steven@stevens-MacBook-Pro-2.local>
2020-03-10 16:03:17 -0400

    cleaned up pylint

--- usage.py
+++ usage.py
@@ -36,15 +36,14 @@ CONTENT_STYLE = {
     "padding": "2rem 1rem",
 }
 content = html.Div([
-    rimble.card(html.H3('Testing card')
-    ),
-    rimble.box(html.H3('test'),style={'bg':'salmon'}),
-    html.Div(id='output', children = rimble.qr(value='hist')),
-    dcc.Input(id='input',type='text',value='test'),
-    html.Div(id="metamask_button", children=rimble.metamask(id='test',value='Connect with MetaMask')),
-    html.Div(id='blockie_output', children = rimble.blockie(id='blockie_test')),
-    dcc.Input(id='blockie_input',type='text'),
-    html.Div(id='ethaddress',children=rimble.ethaddress(address='test',textLabels=True))
+    rimble.card(html.H3('Testing card')),
+    rimble.box(html.H3('test'), style={'bg':'salmon'}),
+    html.Div(id='output', children=rimble.qr(value='hist')),
+    dcc.Input(id='input', type='text', value='test'),
+    html.Div(id="metamask_button", children=rimble.metamask(id='test', value='Connect with MetaMask')),
+    html.Div(id='blockie_output', children=rimble.blockie(id='blockie_test')),
+    dcc.Input(id='blockie_input', type='text'),
+    html.Div(id='ethaddress', children=rimble.ethaddress(address='test', textLabels=True))
 ], style=CONTENT_STYLE)
 
 app.layout = html.Div([sidebar, content])
@@ -56,7 +55,7 @@ def display_output(value):
 
 @app.callback(Output('blockie_output', 'children'), [Input('blockie_input', 'value')])
 def display_blockie(value):
-    return rimble.blockie(id='blockie_test',value=value)
+    return rimble.blockie(id='blockie_test', value=value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
